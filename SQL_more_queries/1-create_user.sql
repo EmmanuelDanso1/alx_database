95e5339d2b2b0b25f31f233264fe9b26a4a2ca4b
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-17 10:15:36 -0700

    creating and granting user privileges with flush

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script gives a particular USER access with a known PASSWORD and runs if user exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
