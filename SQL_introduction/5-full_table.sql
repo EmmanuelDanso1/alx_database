f8b754b476249b2428bb98d0a2d8ebd3912bb3c5
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-17 06:32:34 -0700

    new update

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- This script SHOWS the full table in columns for a specific database
-SELECT id, name, c ,created_at, PRIMARYKEY(id)
-FROM information_schema.columns WHERE table_schema = 'hbtn_0c_0' 
-AND table_name = 'first_table';
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
